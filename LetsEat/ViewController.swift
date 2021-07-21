853cafa9aeb1842f56b6a225a18e5dd8de797b20
Dima <dmitrysinkov6@gmail.com>
2021-07-21 12:09:47 +0300

    Initial Commit

---
+++ LetsEat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsEat
+//
+//  Created by Dmitry on 21.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
