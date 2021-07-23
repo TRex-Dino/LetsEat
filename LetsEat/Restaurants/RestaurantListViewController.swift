364afc2ff676986287089a311c37a291148e7101
Dima <dmitrysinkov6@gmail.com>
2021-07-23 19:53:30 +0300

    git file

---
+++ LetsEat/Restaurants/RestaurantListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantListViewController.swift
+//  LetsEat
+//
+//  Created by Dmitry on 21.07.2021.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+    }
+    
+
+    
+}
