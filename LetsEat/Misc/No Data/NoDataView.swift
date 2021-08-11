f4de4a2e040713db9c1cba46eceabaf13c4cfce1
Dima <dmitrysinkov6@gmail.com>
2021-08-11 22:14:41 +0300

    connected outlets in the RestaurantDetailVC to the Restaurant Detail added methods to viewDidLoad() to populate the table view when the Restaurant Detail screen is displayed

--- LetsEat/Misc/No Data/NoDataView.swift
+++ LetsEat/Misc/No Data/NoDataView.swift
@@ -8,13 +8,40 @@
 import UIKit
 
 class NoDataView: UIView {
+    
+    var view: UIView!
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var lblDesc: UILabel!
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let nib = UINib(nibName: "NoDataView", bundle: Bundle.main)
+        let view = nib.instantiate(withOwner: self, options: nil) [0] as! UIView
+        
+        return view
+    }
+    
+    func setupView() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+    
+    func set(title: String) {
+        lblTitle.text = title
+    }
+    
+    func set(desc: String) {
+        lblDesc.text = desc
+    }
 }
